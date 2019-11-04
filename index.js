ac710eae02b38b5faae9f277cf027b4951314339
bohma <36440902+bohma@users.noreply.github.com>
2019-11-05 00:10:37 +0200

    ..

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var app = express();
 
 app.set('view engine','ejs');
 
+const data = 'hi';// эти данные
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index',{data:data}); // Второй аргумент это данные которые нам нужно передать
 });
+app.get('/create', function (req, res) {
+    res.render('create');
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
